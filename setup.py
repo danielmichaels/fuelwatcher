90aa780b3d00e34f403069d50441bf872f90366c
Dan <dans.address@outlook.com>
2018-04-05 20:09:12 +0000

    Now with setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name = 'fuelwatcher',
+        packages = ['fuelwatcher'],
+        version = '0.1',
+        description = 'A simple XML scraper for FuelWatch.wa.gov.au fuel prices',
+        author = 'Daniel Michaels',
+        author_email = 'dans.address@outlook.com',
+        url = 'https://github.com/danielmichaels/fuelwatcher',
+        download_url = 'https://github.com/danielmichaels/fuelwatcher/archive/0.1.tar.gz',
+        keywords = ['fuelwatch', 'western australia', 'fuel watch rss'],
+        classifiers = []
+)
